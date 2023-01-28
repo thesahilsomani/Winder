44720950340268f0d53c2dcdc760d83ae70b7973
thesahilsomani@gmail.com <53291144+thesahilsomani@users.noreply.github.com>
2023-01-27 19:49:23 -0800

    Initial Commit

---
+++ Winder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Winder
+//
+//  Created by Sahil Somani on 1/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Winder/WinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinderApp.swift
+//  Winder
+//
+//  Created by Sahil Somani on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
